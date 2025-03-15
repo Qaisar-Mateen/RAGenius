b6ac2c02563540e02a14751bb13438b780ddc23c
Qaisar-Mateen <qaisarmateen313@gmail.com>
2025-03-16 01:17:30 +0500

    Add Streamlit app for Groq chat functionality

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from util import *
+from groq import Groq
+
+st.set_page_config(page_icon="🧑‍🏫", layout="wide", page_title="RAGenius")
+
+
+def main():
+
+    st.subheader("Groq Chat Streamlit App", divider="rainbow", anchor=False)
+
+    client = Groq(
+        api_key=st.secrets["GROQ_API_KEY"],
+    )
+
+    # Initialize chat history and selected model
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    if "selected_model" not in st.session_state:
+        st.session_state.selected_model = None
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        avatar = '🤖' if message["role"] == "assistant" else '👨‍💻'
+        with st.chat_message(message["role"], avatar=avatar):
+            st.markdown(message["content"])
+
+
+    if prompt := st.chat_input("Enter your prompt here..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("user", avatar='👨‍💻'):
+            st.markdown(prompt)
+
+        # Fetch response from Groq API
+        try:
+            chat_completion = client.chat.completions.create(
+                model='qwen-qwq-32b',
+                messages=[
+                    {
+                        "role": m["role"],
+                        "content": m["content"]
+                    }
+                    for m in st.session_state.messages
+                ],
+                max_completion_tokens=128000,
+                stream=True
+            )
+
+            # Use the generator function with st.write_stream
+            with st.chat_message("assistant", avatar="🤖"):
+                chat_responses_generator = stream_Chat(chat_completion)
+                full_response = st.write_stream(chat_responses_generator)
+        except Exception as e:
+            st.error(e, icon="🚨")
+
+        # Append the full response to session_state.messages
+        if isinstance(full_response, str):
+            st.session_state.messages.append(
+                {"role": "assistant", "content": full_response})
+        else:
+            # Handle the case where full_response is not a string
+            combined_response = "\n".join(str(item) for item in full_response)
+            st.session_state.messages.append(
+                {"role": "assistant", "content": combined_response})
+            
+if __name__ == "__main__":
+    main()
